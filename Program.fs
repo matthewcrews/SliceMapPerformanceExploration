ab9e494e4541ccbf919983b7f058475cffd2b1dd
Matthew <matthewcrews@gmail.com>
2021-08-20 09:32:46 -0700

    setting up test

--- Program.fs
+++ Program.fs
@@ -3,8 +3,6 @@
 open System
 open SliceMapPerformance.Domain
 open SliceMapPerformance.SliceMap
-open BenchmarkDotNet.Attributes
-open BenchmarkDotNet.Running
 
 
 [<Measure>] type City
@@ -12,93 +10,9 @@ open BenchmarkDotNet.Running
 
 let rng = Random 123
 let numberOfCities = 1_000
-let numberOfTrucks = 1_000
+let numberOfTrucks = 10_000
 let cities = [| for c in 1 .. numberOfCities -> LanguagePrimitives.Int32WithMeasure<City> c |]
 let trucks = [| for t in 1 .. numberOfTrucks -> LanguagePrimitives.Int32WithMeasure<Truck> t|]
-
-
-module Dense =
-    
-    let cityTruckPairs =
-        [| 
-            for c in cities do
-            for t in trucks ->
-                c, t
-        |]
-
-    let costs =
-        SliceMap [|
-            for t in trucks ->
-                t, 100.0 + 10.0 * rng.NextDouble()
-        |]
-
-    let capacity =
-        SliceMap [|
-            for t in trucks ->
-                t, 1_000.0 + 10.0 * rng.NextDouble () 
-        |]
-
-    let decisions =
-        SliceMap2D [|
-            for (c, t) in cityTruckPairs ->
-                let decisionName = DecisionName ($"{c.ToString()}_{t.ToString()}")
-                c, t, { Name = decisionName; Type = DecisionType.Boolean }
-        |]
-
-    let loop () =
-        let mutable result = LanguagePrimitives.GenericZero
-
-        for _ = 1 to 10 do
-            for c in cities do
-                let total = sum (capacity .* decisions.[c, All] .* costs)
-                result <- total
-
-        result
-
-
-module MediumSparsity =
-
-    let densityPercent = 0.10
-
-    let cityTruckPairs =
-        [| 
-            for c in cities do
-            for t in trucks ->
-                if rng.NextDouble() < densityPercent then
-                    Some (c, t)
-                else
-                    None
-        |] |> Array.choose id
-
-    let costs =
-        SliceMap [|
-            for t in trucks ->
-                t, 100.0 + 10.0 * rng.NextDouble()
-        |]
-
-    let capacity =
-        SliceMap [|
-            for t in trucks ->
-                t, 1_000.0 + 10.0 * rng.NextDouble () 
-        |]
-
-    let decisions =
-        SliceMap2D [|
-            for (c, t) in cityTruckPairs ->
-                let decisionName = DecisionName ($"{c.ToString()}_{t.ToString()}")
-                c, t, { Name = decisionName; Type = DecisionType.Boolean }
-        |]
-
-    let loop () =
-        let mutable result = LanguagePrimitives.GenericZero
-
-        for _ = 1 to 10 do
-            for c in cities do
-                let total = sum (capacity .* decisions.[c, All] .* costs)
-                result <- total
-
-        result
-
 
 module HighSparsity =
     
@@ -136,59 +50,19 @@ module HighSparsity =
     let loop () =
         let mutable result = LanguagePrimitives.GenericZero
 
-        for _ = 1 to 10 do
-            for c in cities do
-                let total = sum (capacity .* decisions.[c, All] .* costs)
-                result <- total
+        for c in cities do
+            let total = sum (capacity .* decisions.[c, All] .* costs)
+            result <- total
 
         result
 
-
-[<MemoryDiagnoser>]
-type Benchmarks () =
-
-    [<Benchmark>]
-    member _.DenseData () =
-        Dense.loop ()
-
-    [<Benchmark>]
-    member _.MediumSparsity () =
-        MediumSparsity.loop ()
-
-    [<Benchmark>]
-    member _.HighSparsity () =
-        HighSparsity.loop ()
     
 
 [<EntryPoint>]
 let main argv =
 
-    match argv.[0].ToLower() with
-    | "benchmark" ->
-        let summary = BenchmarkRunner.Run<Benchmarks>()
+    for _ = 1 to 1_000 do
+        let _ = HighSparsity.loop ()
         ()
-
-    | "profiledense" ->
-        let iterations = int argv.[1]
-        printfn "Starting loops"
-        for _ = 1 to iterations do
-            let _ = Dense.loop ()
-            ()
-
-    | "profilemedium" ->
-        let iterations = int argv.[1]
-        printfn "Starting loops"
-        for _ = 1 to iterations do
-            let _ = MediumSparsity.loop ()
-            ()
-
-    | "profilesparse" ->
-        let iterations = int argv.[1]
-        printfn "Starting loops"
-        for _ = 1 to iterations do
-            let _ = HighSparsity.loop ()
-            ()
-
-    | _ -> failwith "Invalid workload"
     
     0 // return an integer exit code
